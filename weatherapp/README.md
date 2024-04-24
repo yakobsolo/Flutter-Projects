db2b1803d572ef98caf9526cb426006c254aad8d
yakobsolo <yakisolo2009@gmail.com>
2024-04-24 17:44:20 +0300

    fit(homepage)

---
+++ weatherapp/README.md
@@ -0,0 +1,21 @@
+# Weather App
+
+In this project i have learned how to work is OpenWeather api.
+
+
+## Features
+
+- **Home Page**: 
+    - Displays the city where you are.
+    - Displays the tempreture of the city you are in degree celsius 
+    - Displays the condition of the weather.
+    - Displays lottie animation based on the weather condition.
+    
+
+
+### Project Demo
+
+https://github.com/yakobsolo/Flutter-Projects/assets/99880602/cf782a78-41f7-4ddb-8e9c-125f9035310c
+
+
+
--- weatherapp/lib/features/wetherepage/UI/weatherscrean.dart
+++ weatherapp/lib/features/wetherepage/UI/weatherscrean.dart
@@ -1,4 +1,8 @@
+
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weatherapp/features/wetherepage/data/weatherdata.dart';
+import 'package:weatherapp/features/wetherepage/model/weathermodel.dart';
 
 class WeatherApp extends StatefulWidget {
   const WeatherApp({super.key});
@@ -8,8 +12,75 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
+  final _weatherdata = WeatherData(apiKey: "d0eec2825917e966697fc9679502a142");
+
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherdata.getCurrentCity();
+
+    try {
+      final weather = await _weatherdata.getWeather(cityName);
+
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    _fetchWeather();
+    super.initState();
+  }
+
+  String getWeatherAnimation(String? mainCondition) {
+      if (mainCondition == null) return  "assets/sunny.json";
+      switch (mainCondition.toLowerCase()) {
+        case 'clouds':
+        case 'mist':
+        case 'smoke':
+        case 'haze':
+        case 'dust':
+        case 'fog':
+          return "assets/cloudy.json";
+          
+        case 'rain':
+        case 'drizzle':
+        case 'shower rain':
+          return "assets/thunder.json";
+
+        case 'thunderstorm':
+          return "assets/thunder.json";
+
+        
+
+        default:
+          return "assets/sunny.json";
+      }
+  } 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_weather?.cityName?? "Loading city... oops check your network", style: TextStyle( overflow: TextOverflow.clip),),
+              Lottie.asset(getWeatherAnimation(_weather?.mainCondition?? "")),
+              SizedBox(height: 20,),
+              Text("${_weather?.tempreture.round()} C"),
+              Text(_weather?.mainCondition?? ""),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
